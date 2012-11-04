e58509356fb65cdb8adbc0e8b76077babb81d121
Rich Loveland <loveland.richard@gmail.com>
2012-11-04 11:05:03 -0500

    Changing to ``External Scheme REPL'' mode.

--- inf-scheme.scm
+++ inf-scheme.scm
@@ -19,27 +19,24 @@ along with MIT/GNU Scheme; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301,
 USA.
 
-I've plagiarized both Olin Shivers' `cmuscheme.el' and `iuscheme.el' by
-Chris Haynes and Erik Hilsdale, as well as `shell.scm' from Edwin itself.
-
 |#
 
-;;;; Scsh subprocess in a buffer
+;;;; External Scheme subprocess in a buffer
 
 (declare (usual-integrations))
 
-(define-variable inferior-scheme-prompt-pattern
-  "Regexp to match prompts in the inferior Scheme."
+(define-variable external-scheme-prompt-pattern
+  "Regexp to match prompts in the external Scheme."
   (os/default-shell-prompt-pattern)
   string?)
 
-(define-variable explicit-inferior-scheme-file-name
-  "If not #f, file name to use for explicitly requested inferior Scheme."
+(define-variable explicit-external-scheme-file-name
+  "If not #f, file name to use for explicitly requested external Scheme."
   #f
   string-or-false?)
 
-(define-major-mode inferior-scheme comint "Inferior Scheme"
-  "Major mode for interacting with an inferior Scheme.
+(define-major-mode exterior-scheme-repl comint "Inferior Scheme"
+  "Major mode for interacting with an external Scheme.
 Return after the end of the process' output sends the text from the 
     end of process to the end of the current line.
 
@@ -47,42 +44,42 @@ If you accidentally suspend your process, use \\[comint-continue-subjob]
 to continue it.
 
 Customisation: Entry to this mode runs the hooks on comint-mode-hook and
-inferior-scheme-mode-hook (in that order)."
+external-scheme-mode-hook (in that order)."
   (lambda (buffer)
     (local-set-variable! comint-prompt-regexp
-			 (ref-variable inferior-scheme-prompt-pattern buffer)
+			 (ref-variable external-scheme-prompt-pattern buffer)
 			 buffer)
     (local-set-variable! local-abbrev-table
-			 (ref-variable inferior-scheme-mode-abbrev-table buffer)
+			 (ref-variable external-scheme-mode-abbrev-table buffer)
 			 buffer)
     (event-distributor/invoke!
-     (ref-variable inferior-scheme-mode-hook buffer) buffer)))
+     (ref-variable external-scheme-mode-hook buffer) buffer)))
 
-(define-variable inferior-scheme-mode-abbrev-table
-  "Mode-specific abbrev table for Inferior Scheme mode.")
+(define-variable external-scheme-mode-abbrev-table
+  "Mode-specific abbrev table for External Scheme mode.")
 (define-abbrev-table 'shell-mode-abbrev-table '())
 
-(define-variable inferior-scheme-mode-hook
+(define-variable external-scheme-mode-hook
   "An event distributor that is invoked when entering Inferior Scheme mode."
   (make-event-distributor))
 
-(define-key 'inferior-scheme #\tab 'lisp-indent-line)
-(define-key 'inferior-scheme #\) 'lisp-insert-paren)
-(define-key 'inferior-scheme #\c-m-q 'indent-sexp)
+(define-key 'external-scheme #\tab 'lisp-indent-line)
+(define-key 'external-scheme #\) 'lisp-insert-paren)
+(define-key 'external-scheme #\c-m-q 'indent-sexp)
 
 
-(define-command run-scheme
+(define-command external-scheme-repl
   "Run an inferior Scheme, with I/O through buffer *inferior-scheme*.
 With prefix argument, unconditionally create a new buffer and process.
 If buffer exists but Scheme process is not running, make new shell.
 If buffer exists and Scheme process is running, just switch to buffer
-  *inferior-scheme*. 
+  *external-scheme-repl*. 
 
 The location of the Scheme binary to use comes from either (1) the
-variable `explicit-inferior-scheme-file-name' or (2) the
-INFERIOR_SCHEME environment variable.
+variable `explicit-external-scheme-file-name' or (2) the
+PREFERRED_SCHEME environment variable.
 
-The buffer is put in Inferior Scheme mode, giving commands for sending
+The buffer is put in External Scheme mode, giving commands for sending
 input."
   "sRun Scheme: \nP"
   (lambda (scheme-program-name new-buffer?)
@@ -90,12 +87,13 @@ input."
      (let ((program
 	    (or
 	     scheme-program-name
-	     (ref-variable explicit-inferior-scheme-file-name)
-	     (get-environment-variable "INFERIOR_SCHEME"))))
+	     (ref-variable explicit-external-scheme-file-name)
+	     (get-environment-variable "PREFERRED_SCHEME"))))
        (apply make-comint
-	      (ref-mode-object inferior-scheme)
+	      (ref-mode-object exterior-scheme-repl)
 	      (if (not new-buffer?)
-		  "*inferior-scheme*"
-		  (new-buffer "*inferior-scheme*"))
+		  "*exterior-scheme-repl*"
+		  (new-buffer "*exterior-scheme-repl*"))
 	      program
 	      '())))))
+
